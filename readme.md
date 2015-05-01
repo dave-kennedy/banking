f5dd3b517add1a21f50caff13a1f7eddc6924f04
Pajamaman <Pajamaman@users.noreply.github.com>
2015-05-01 16:51:21 -0600

    Removed dependency on mysql

--- readme.md
+++ readme.md
@@ -1,69 +1,50 @@
 This is just a little script I use to figure out where my money goes.
 
-First, create the database and schema:
+I export my banking transactions as a csv file from my bank's website, and I maintain a list of categories in a separate csv file. Samples of these files can be found in the data subfolder.
 
-```bash
-mysql> create database banking;
-mysql> use banking;
-mysql> source '/path/to/schema.sql';
+The transactions file should have columns for the date, amount and name of each transaction, and a header row:
+
+```
+"Date","Name","Amount"
+"1/4/2015","DEPOSIT","1000000.00"
 ```
 
-To get data into the database, I export my banking transactions as a csv file from my bank's website. Then I import it in mysql (after starting the mysql shell with `--local-infile`):
+The categories file should have columns for the name of each category and a list of matching keywords as a regular expression:
 
-```bash
-mysql> load data local infile '/path/to/transactions.csv' into table transactions fields terminated by ',' enclosed by '"' lines terminated by '\n' (date, description, `check`, credit, debit);
+```
+"Name","Keywords"
+"Deposits","^deposit$"
 ```
 
-A sample is provided to get started with (in the data subfolder), as well as sample categories and keywords. A sample config file is also provided (in the config subfolder). This file needs to be renamed to config.json and updated with the correct values.
-
-Next, install the dependencies:
+The script can then be run like so:
 
 ```bash
-$ npm install
+$ node transactions.js data/transactions.csv data/categories.csv
 ```
 
-Finally, run the script:
-
-```bash
-$ node transactions.js
-```
-
-If a transaction doesn't match any of the keywords, the script will throw an exception. For example:
+If a transaction doesn't match any of the categories, the script will throw an exception:
 
 ```bash
 Transaction not categorized:
-Transaction ID: 300
-Date: Wed Apr 16 2014 00:00:00
-Description: Debit Purchase -visa Amazon Mktplace Amzn.com
-Check: N/A
-Credit: 0.00
-Debit: 30.42
+Date: Tuesday, January 6, 2015
+Description: DEBIT PURCHASE -VISA GOG.COM
+Amount: -2.49
 ```
 
-Knowing this was an electronics purchase, I can add a category like so:
+Knowing this was a games purchase, I can add a category to the categories file:
 
-```bash
-$ node transactions.js --add-category "Electronics"
 ```
-
-Then I can add a keyword:
-
-```bash
-$ node transactions.js --add-keyword "amzn\.com" --to-category "electronics"
+"Games","gog\.com|steamgames\.com"
 ```
-
-The keywords are matched by regular expression, so special characters have to be escaped.
 
 Once every transaction has been categorized, the script will print out a summary of each category to the console:
 
 ```bash
-Category ID: 10
-Name: Electronics
-Keywords: amzn\.com
-Total transactions: 1
-Total debits: 30.42
-Total credits: 0.00
+Category: Games
+Keywords: gog\.com, steamgames\.com
+Total transactions: 2
+Total amount: -4.98
 ```
 
-Use the `--inspect-category` option, followed by the name of a category, to see the detailed output. `--list-categories` will list categories and `--list-keywords` will list keywords.
+Use the `--inspect-category` option, followed by the name of a category, to see the transactions in that category. A date range can be specified with the `--from-date` and `--to-date` options.
 
--- transactions.js
+++ transactions.js
@@ -1,357 +1,189 @@
-var fs = require('fs'),
-    mysql = require('mysql');
+var fs = require('fs');
 
-function Category(id, name) {
-    if (!id || !name) {
-        throw 'Cannot create category without ID and name';
+function Category(name, keywords) {
+    if (!name || !keywords) {
+        throw 'Cannot create category without name and keywords';
     }
 
-    this.id = id;
     this.name = name;
-    this.keywords = [];
+    this.keywords = new RegExp(keywords, 'i');
     this.transactions = [];
-    this.regexp = null;
     this.totalTransactions = 0;
-    this.totalDebits = 0;
-    this.totalCredits = 0;
+    this.totalAmount = 0;
 }
-
-Category.prototype.addKeyword = function (keyword) {
-    this.keywords.push(keyword);
-    this.regexp = new RegExp(this.getKeywords('|'), 'i');
-};
 
 Category.prototype.addTransaction = function (transaction) {
     this.transactions.push(transaction);
     this.totalTransactions++;
-    this.totalDebits += transaction.debit;
-    this.totalCredits += transaction.credit;
-};
-
-Category.prototype.hasKeyword = function (keyword) {
-    return this.id == keyword.categoryID;
-};
-
-Category.prototype.getKeywords = function (separator) {
-    return this.keywords.map(function (keyword) {
-        return keyword.name;
-    }).join(separator);
+    this.totalAmount += transaction.amount;
 };
 
 Category.prototype.matchTransaction = function (transaction) {
-    return this.regexp.test(transaction.description);
+    return this.keywords.test(transaction.description);
 };
 
-Category.prototype.toString = function () {
-    return 'Category ID: ' + this.id + '\n' +
-           'Name: ' + this.name + '\n' +
-           'Keywords: ' + this.getKeywords(', ') + '\n' +
-           'Total transactions: ' +  this.totalTransactions + '\n' +
-           'Total debits: ' + this.totalDebits.toFixed(2) + '\n' +
-           'Total credits: ' + this.totalCredits.toFixed(2) + '\n';
+Category.prototype.toString = function (verbose) {
+    var output = 'Category: ' + this.name + '\n' +
+                 'Keywords: ' + this.keywords.source.replace(/\|/g, ', ') + '\n' +
+                 'Total transactions: ' +  this.totalTransactions + '\n' +
+                 'Total amount: ' + this.totalAmount.toFixed(2) + '\n\n';
+
+    if (verbose) {
+        this.transactions.sort(function (transactionA, transactionB) {
+            if (transactionA.date < transactionB.date) {
+                return -1;
+            }
+
+            if (transactionA.date > transactionB.date) {
+                return 1;
+            }
+
+            return 0;
+        }).forEach(function (transaction) {
+            output += transaction.toString();
+        });
+    }
+
+    return output;
 };
 
-function Keyword(id, name, categoryID) {
-    if (!id || !name || !categoryID) {
-        throw 'Cannot create keyword without ID, name and category ID';
+function Transaction(date, description, amount) {
+    if (!date || !description || !amount) {
+        throw 'Cannot create transaction without date, description and amount';
     }
 
-    this.id = id;
-    this.name = name;
-    this.categoryID = categoryID;
-}
-
-function Transaction(id, date, description, check, credit, debit) {
-    if (!id || !date || !description) {
-        throw 'Cannot create transaction without ID, date and description';
-    }
-
-    this.id = id;
-    this.date = date;
-    this.description = description;
-    this.check = check || 'N/A';
-    this.credit = credit || 0;
-    this.debit = debit || 0;
+    this.date = new Date(date);
+    this.description = description.trim();
+    this.amount = parseFloat(amount);
 }
 
 Transaction.prototype.toString = function () {
-    return 'Transaction ID: ' + this.id + '\n' +
-           'Date: ' + this.date + '\n' +
+    return 'Date: ' + this.date.toLocaleDateString() + '\n' +
            'Description: ' + this.description + '\n' +
-           'Check: ' + this.check + '\n' +
-           'Credit: ' + this.credit.toFixed(2) + '\n' +
-           'Debit: ' + this.debit.toFixed(2) + '\n';
+           'Amount: ' + this.amount.toFixed(2) + '\n\n';
 };
 
-function readConfig(callback) {
-    fs.readFile('config/config.json', 'utf-8', function (err, data) {
+function getOptions() {
+    var options = {};
+
+    if (!process.argv[2]) {
+        throw 'Missing name of csv file containing transaction data';
+    }
+
+    options.transactionFile = process.argv[2];
+
+    if (!process.argv[3]) {
+        throw 'Missing name of csv file containing category data';
+    }
+
+    options.categoryFile = process.argv[3];
+
+    if (process.argv.indexOf('--inspect-category') > -1) {
+        options.inspectCategory = process.argv[process.argv.indexOf('--inspect-category') + 1];
+    }
+
+    if (process.argv.indexOf('--from-date') > -1) {
+        options.fromDate = new Date(process.argv[process.argv.indexOf('--from-date') + 1]);
+    }
+
+    if (process.argv.indexOf('--to-date') > -1) {
+        options.toDate = new Date(process.argv[process.argv.indexOf('--to-date') + 1]);
+    }
+
+    return options;
+}
+
+function isNotEmpty(str) {
+    return str && str != ',';
+}
+
+function getData(filename, callback) {
+    fs.readFile(filename, 'utf-8', function (err, data) {
         if (err) {
             throw err;
         }
 
-        var config = JSON.parse(data);
+        var lines = data.split('\n').filter(isNotEmpty),
+            header = lines.shift(),
+            columns = header.split('"').filter(isNotEmpty),
+            rows = lines.map(function (line, index) {
+                var row = line.split('"').filter(isNotEmpty);
 
-        callback(config);
-    });
-}
+                if (row.length != columns.length) {
+                    throw 'Columns mismatch on line ' + (index + 2) + ':\n' + line + '\n';
+                }
 
-function createConnection(config) {
-    return mysql.createConnection({
-        host: config.host,
-        user: config.user,
-        password: config.password,
-        database: config.database,
-        multipleStatements: true
+                return row;
+            }),
+            objects = rows.map(function (row) {
+                var obj = {};
+
+                columns.forEach(function (column, index) {
+                    obj[column] = row[index];
+                });
+
+                return obj;
+            });
+
+        callback(objects);
     });
 }
 
 function displayCategories(categories, verbose) {
     categories.forEach(function (category) {
-        console.log(category.toString());
+        process.stdout.write(category.toString(verbose));
+    });
+}
 
-        if (verbose) {
-            category.transactions.forEach(function (transaction) {
-                console.log(transaction.toString());
+(function () {
+    var options = getOptions();
+
+    getData(options.transactionFile, function (transactions) {
+        transactions = transactions.map(function (transaction) {
+            return new Transaction(transaction.Date, transaction.Name, transaction.Amount);
+        });
+
+        if (options.fromDate || options.toDate) {
+            transactions = transactions.filter(function (transaction) {
+                if (options.fromDate && transaction.date < options.fromDate) {
+                    return false;
+                }
+
+                if (options.toDate && transaction.date > options.toDate) {
+                    return false;
+                }
+
+                return true;
             });
         }
-    });
-}
 
-function getData(connection, categoryName, fromDate, toDate) {
-    var sql = 'SELECT * FROM categories ORDER BY name;' +
-              'SELECT * FROM keywords ORDER BY category_id, name;' +
-              'SELECT * FROM transactions ',
-        where = [],
-        values = [];
+        getData(options.categoryFile, function (categories) {
+            categories = categories.map(function (category) {
+                return new Category(category.Name, category.Keywords);
+            });
 
-    if (fromDate) {
-        where.push('date >= ?');
-        values.push(new Date(fromDate));
-    }
+            transactions.forEach(function (transaction) {
+                for (var c = 0; c < categories.length; c++) {
+                    category = categories[c];
 
-    if (toDate) {
-        where.push('date <= ?');
-        values.push(new Date(toDate));
-    }
-
-    if (where.length) {
-        sql += 'WHERE ' + where.join(' AND ') + ' ';
-    }
-
-    sql += 'ORDER BY date';
-
-    connection.connect();
-
-    connection.query(sql, values, function (err, rows) {
-        if (err) {
-            throw err;
-        }
-
-        var categoryRows = rows[0],
-            keywordRows = rows[1],
-            transactionRows = rows[2];
-
-        var categories = categoryRows.map(function (row) {
-            return new Category(row.id, row.name);
-        });
-
-        var keywords = keywordRows.map(function (row) {
-            return new Keyword(row.id, row.name, row.category_id);
-        });
-
-        keywords.forEach(function (keyword) {
-            for (var c = 0; c < categories.length; c++) {
-                category = categories[c];
-
-                if (category.hasKeyword(keyword)) {
-                    return category.addKeyword(keyword);
+                    if (category.matchTransaction(transaction)) {
+                        return category.addTransaction(transaction);
+                    }
                 }
-            }
-        });
 
-        var transactions = transactionRows.map(function (row) {
-            return new Transaction(row.id, row.date, row.description, row.check, row.credit, row.debit);
-        });
+                throw 'Transaction not categorized:\n' + transaction.toString();
+            });
 
-        transactions.forEach(function (transaction) {
-            for (var c = 0; c < categories.length; c++) {
-                category = categories[c];
+            if (options.inspectCategory) {
+                categories = categories.filter(function (category) {
+                    return category.name.toLowerCase() == options.inspectCategory.toLowerCase();
+                });
 
-                if (category.matchTransaction(transaction)) {
-                    return category.addTransaction(transaction);
-                }
+                return displayCategories(categories, true);
             }
 
-            throw 'Transaction not categorized:\n' + transaction.toString();
-        });
-
-        if (categoryName) {
-            displayCategories(categories.filter(function (category) {
-                return category.name.toLowerCase() == categoryName.toLowerCase();
-            }), true);
-        } else {
             displayCategories(categories);
-        }
-
-        connection.end();
-    });
-}
-
-function addCategory(connection, categoryName) {
-    if (!categoryName) {
-        throw 'Cannot add category without name';
-    }
-
-    var sql = 'INSERT INTO categories SET ?';
-
-    connection.connect();
-
-    connection.query(sql, {name: categoryName}, function (err, result) {
-        if (err) {
-            throw err;
-        }
-
-        console.log('Category "' + categoryName + '" added');
-
-        connection.end();
-    });
-}
-
-function addKeyword(connection, keywordName, categoryName) {
-    if (!keywordName || !categoryName) {
-        throw 'Cannot add keyword without name and category name';
-    }
-
-    var sql = 'SELECT id FROM categories WHERE ?';
-
-    connection.connect();
-
-    connection.query(sql, {name: categoryName}, function (err, rows) {
-        if (err) {
-            throw err;
-        }
-
-        if (!rows.length) {
-            throw 'Category "' + categoryName + '" not found';
-        }
-
-        var categoryID = rows[0].id,
-            sql = 'INSERT INTO keywords SET ?';
-
-        connection.query(sql, {name: keywordName, category_id: categoryID}, function (err, result) {
-            if (err) {
-                throw err;
-            }
-
-            console.log('Keyword "' + keywordName + '" added to category "' + categoryName + '"');
-
-            connection.end();
         });
     });
-}
-
-function listCategories(connection) {
-    var sql = 'SELECT * FROM categories ORDER BY name';
-
-    connection.connect();
-
-    connection.query(sql, function (err, rows) {
-        if (err) {
-            throw err;
-        }
-
-        rows.forEach(function (row) {
-            console.log(row);
-        });
-
-        connection.end();
-    });
-}
-
-function listKeywords(connection, categoryName) {
-    var sql = 'SELECT k.id, k.name AS keyword, c.name AS category ' +
-              'FROM keywords k ' +
-              'JOIN categories c ON k.category_id = c.id ',
-        values = [];
-
-    if (categoryName) {
-        sql += 'WHERE c.name = ? ';
-        values.push(categoryName);
-    }
-
-    sql += 'ORDER BY category, keyword';
-
-    connection.connect();
-
-    connection.query(sql, values, function (err, rows) {
-        if (err) {
-            throw err;
-        }
-
-        rows.forEach(function (row) {
-            console.log(row);
-        });
-
-        connection.end();
-    });
-}
-
-function invoke(config) {
-    var connection = createConnection(config);
-
-    if (process.argv.indexOf('--add-category') > -1) {
-        if (process.argv.indexOf('--add-keyword') > -1
-            || process.argv.indexOf('--list-categories') > -1
-            || process.argv.indexOf('--list-keywords') > -1
-            || process.argv.indexOf('--inspect-category') > -1
-            || process.argv.indexOf('--from-date') > -1
-            || process.argv.indexOf('--to-date') > -1) {
-            throw 'Cannot use --add-category with any other options';
-        }
-
-        return addCategory(connection, process.argv[process.argv.indexOf('--add-category') + 1]);
-    }
-
-    if (process.argv.indexOf('--add-keyword') > -1 && process.argv.indexOf('--to-category') > -1) {
-        if (process.argv.indexOf('--list-categories') > -1
-            || process.argv.indexOf('--list-keywords') > -1
-            || process.argv.indexOf('--inspect-category') > -1
-            || process.argv.indexOf('--from-date') > -1
-            || process.argv.indexOf('--to-date') > -1) {
-            throw 'Cannot use --add-keyword with any other options';
-        }
-
-        return addKeyword(connection,
-            process.argv[process.argv.indexOf('--add-keyword') + 1],
-            process.argv[process.argv.indexOf('--to-category') + 1]);
-    }
-
-    if (process.argv.indexOf('--list-categories') > -1) {
-        if (process.argv.indexOf('--list-keywords') > -1
-            || process.argv.indexOf('--inspect-category') > -1
-            || process.argv.indexOf('--from-date') > -1
-            || process.argv.indexOf('--to-date') > -1) {
-            throw 'Cannot use --list-categories with any other options';
-        }
-
-        return listCategories(connection);
-    }
-
-    if (process.argv.indexOf('--list-keywords') > -1) {
-        if (process.argv.indexOf('--inspect-category') > -1
-            || process.argv.indexOf('--from-date') > -1
-            || process.argv.indexOf('--to-date') > -1) {
-            throw 'Cannot use --list-keywords with any other options';
-        }
-
-        return listKeywords(connection, process.argv[process.argv.indexOf('--list-keywords') + 1]);
-    }
-
-    getData(connection,
-        process.argv.indexOf('--inspect-category') > -1 ? process.argv[process.argv.indexOf('--inspect-category') + 1] : null,
-        process.argv.indexOf('--from-date') > -1 ? process.argv[process.argv.indexOf('--from-date') + 1] : null,
-        process.argv.indexOf('--to-date') > -1 ? process.argv[process.argv.indexOf('--to-date') + 1] : null);
-}
-
-readConfig(invoke);
+}());
 
